01c47cde35c4025791d59a92a396a0949285c101
Kevin Casey <kacasey@berkeley.edu>
2014-04-06 17:08:24 -0700

    attempting xml

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import logging
 from google.appengine.api import urlfetch
 import urllib
 import urlparse
+from xml.etree import ElementTree as et
 
 from environment_variables import *
  
@@ -30,7 +31,7 @@ jinja_environment = jinja2.Environment(
 def query(q, fl="id"):
     BASE_URL = 'https://apis.berkeley.edu/solr/fsm/select'
     url = "{base_url}?".format(base_url=BASE_URL) + urllib.urlencode({'q':q,
-                          'fl':fl,
+                          #'fl':fl,
                           'wt':'python',
                           'app_id':FSM_APP_ID,
                           'app_key':FSM_APP_KEY})
@@ -61,8 +62,6 @@ def appendToQuery(q, elem):
         return elem
     return q + ' AND ' + elem
 
-
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {}
@@ -83,10 +82,34 @@ class SearchHandler(webapp2.RequestHandler):
 class ArticleHandler(webapp2.RequestHandler):
     def get(self):
         myId = self.request.get("id")
+        myId = escapeAndFixId(myId)
         info = eval(find(myId))
-        image_link = info["response"]["docs"][0]["fsmImageUrl"][-1]
+        info = info["response"]["docs"][0] # get the first doc (should only be one)
+        del info['id'] # don't display id
         template_values = {}
-        template_values['picture_link'] = image_link
+        if "fsmImageUrl" in info:
+            image_link = info["fsmImageUrl"][-1]
+            del info['fsmImageUrl']
+            template_values['picture_link'] = image_link
+        else:
+            teiUrl = info["fsmTeiUrl"][-1]
+            r = urlfetch.fetch(teiUrl).content
+            xml = et.fromstring(r)
+            text = xml.getElementsByTagName("text")[0]
+            def dump(e):
+                ret_val =  '<%s>' % e.tag
+                if e.text:
+                    ret_val += e.text
+                for n in e:
+                    ret_val += dump(n)
+                ret_val += '</%s>' % e.tag
+                if e.tail:
+                    ret_val += e.tail
+                return ret_val
+
+            template_values['content'] = dump(text)
+
+        template_values['results'] = info
         template = jinja_environment.get_template("article.html")
         self.response.out.write(template.render(template_values))
       
