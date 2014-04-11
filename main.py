263a18cbd751b80372b4955a20f1feb10362171b
alice <aliceworkmail@gmail.com>
2014-04-11 13:15:43 -0700

    deleted webapp2 stuff

--- main.py
+++ main.py
@@ -1,34 +1,5 @@
 #!/usr/bin/env python
 
-"""import webapp2
-import jinja2
-import os
-import logging
-
-from searchHandler import SearchHandler
-from articleHandler import ArticleHandler
-from snippetHandler import SnippetHandler
-from audioHandler import AudioHandler
-from audioSearchHandler import AudioSearchHandler
- 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        template_values = {}
-        template = jinja_environment.get_template("home.html")
-        self.response.out.write(template.render(template_values))
-      
-app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/search', SearchHandler),
-    ('/article', ArticleHandler),
-    ('/find_snippets', SnippetHandler),
-    ('/audioId', AudioHandler),
-    ('/audioSearch', AudioSearchHandler)
-], debug=True)"""
-
 import sys, os
 
 package_dir = "packages"
