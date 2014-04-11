0010538d94d6542c6733453e81cdf1f9fd1fa778
Kevin Casey <kacasey@berkeley.edu>
2014-04-11 13:55:24 -0700

    audio search back

--- audioSearchHandler.py
+++ audioSearchHandler.py
@@ -6,11 +6,30 @@ from xml.etree import ElementTree as et
 
 from helper import *
 
-from bottle import get, route, run, TEMPLATE_PATH, jinja2_template as template
+from bottle import request, route, TEMPLATE_PATH, jinja2_template as template
 
 @route('/audioSearch')
 def audioSearchHandler():
-    return template("search.html")
+    q = request.query["q"]
+    if not q:
+        q = "[* TO *]" # blank query, collect all
+    collection = request.query.collection
+    if not collection:
+        collection = 1712
+    else:
+        collection = int(collection)
+    results = popup(q, collection)
+    if 'results' in results.keys():
+        info = results['results']
+    else:
+        info = ""
+    template_values = {}
+    template_values["response"] = info
+    template_values["collection"] = collection
+    template_values["typeOfResource"] = "audio"
+    template_values["numPages"] = 1
+    template_values["query"] = request.query["q"]
+    return template("search.html", template_values)
 
 """
 class AudioSearchHandler(webapp2.RequestHandler):
