e94cbdac18c024204755d0f976e1a6f6844095b2
Kevin Casey <kacasey@berkeley.edu>
2014-04-11 13:50:19 -0700

    snippets have returned

--- snippetHandler.py
+++ snippetHandler.py
@@ -3,11 +3,51 @@ from xml.etree import ElementTree as et
 import json
 import re
 
-from bottle import get, route, run, TEMPLATE_PATH, jinja2_template as template
+from bottle import request, response, route, TEMPLATE_PATH, jinja2_template as template
 
 @route('/find_snippets')
 def snippetHandler():
-    return json.dumps({})
+    query = request.query['query']
+    queryLower = query.lower()
+
+    teiUrl = request.query["fsmTeiUrl"]
+    r = urllib2.urlopen(teiUrl).read()
+    xml = et.fromstring(r)
+    text = xml.findall("text")[0] # ignore the header
+
+    targets = []
+
+    def acquireTargets(e):
+        if e.text and e.text.lower().find(queryLower) != -1:
+            targets.append(e.text)
+        for n in e:
+            acquireTargets(n)
+        if e.tail and e.tail.lower().find(queryLower) != -1:
+            targets.append(e.tail)
+
+    acquireTargets(text)
+
+    response.add_header('content-type', 'application/json')
+
+    if len(targets) != 0:
+        # use the first match and highlight it
+        target = targets[0]
+        subbedTarget = re.sub(query, "<mark>" + query + "</mark>", target, flags=re.IGNORECASE)
+        myResponse = {'snippet':subbedTarget, 'matches':len(targets), 'fsmTeiUrl':teiUrl}
+        return json.dumps(myResponse)
+    else:
+        # there were no pure matches.  Just get something so the user has a snippet
+        something = []
+        def acquireSomething(e):
+            if len(something): # we got something, halt the recursion
+                return
+            if e.text and len(e.text.strip()) > 20: # make sure we don't get something short
+                something.append(e.text)
+            for n in e:
+                acquireSomething(n)
+        acquireSomething(text)
+        myResponse = {'snippet':something[0], 'matches':0, 'fsmTeiUrl':teiUrl}
+        return json.dumps(myResponse)
 
 """
 class SnippetHandler(webapp2.RequestHandler):
