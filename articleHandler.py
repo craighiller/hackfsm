802d233b7cd650498aef10d3d924e6a8846a8448
Kevin Casey <kacasey@berkeley.edu>
2014-04-11 13:53:11 -0700

    article bacl

--- articleHandler.py
+++ articleHandler.py
@@ -8,11 +8,28 @@ from xml.etree import ElementTree as et
 from helper import *
 
 
-from bottle import get, route, run, TEMPLATE_PATH, jinja2_template as template
+from bottle import request, route, jinja2_template as template
 
 @route('/article')
 def articleHandler():
-    return template("search.html")
+    myId = request.query["id"].replace(':', '\:') # escape the colon metacharacter
+    info = find(myId)["response"]["docs"][0] # get the first doc (should only be one)
+    del info['id'] # del keys so we don't display them at the bottom
+    template_values = {}
+    if "fsmImageUrl" in info:
+        image_link = info["fsmImageUrl"][-1]
+        del info['fsmImageUrl']
+        template_values['picture_link'] = image_link
+    else:
+        teiUrl = info["fsmTeiUrl"][-1]
+        del info['fsmTeiUrl']
+        r = urllib2.urlopen(teiUrl).read()
+        xml = et.fromstring(r)
+        text = xml.findall("text")[0] # ignore the TEI header, only get content
+        template_values['content'] = xmlToHTML(text)
+
+    template_values['results'] = info
+    return template("article.html", template_values)
 
 """
 class ArticleHandler(webapp2.RequestHandler):
